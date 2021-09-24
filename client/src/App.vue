6999c033ba9b193df1172f5d1030e183081dc839
Benjamin-Gruber <83592290+Benjamin-Gruber@users.noreply.github.com>
2021-09-24 15:15:17 +0200

    Cola Logo wurde eingefügt

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,11 +1,11 @@
 <template>
- <v-app>
-<v-app-bar class="blue lighten-5" app>
-<span class="text-h4">Drink</span>
-<v-img class="mx-2" src="images/logo.svg" max-height="140" max-width="140" contain></v-img>
-</v-app-bar>
-<v-main class="blue"> </v-main>
-</v-app>
+  <v-app>
+    <v-app-bar class="blue lighten-5" app>
+      <span class="text-h4">Drink</span>
+      <v-img class="mx-2" src="../Cola.png" max-height="140" max-width="140" contain></v-img>
+    </v-app-bar>
+    <v-main class="blue"> </v-main>
+  </v-app>
 </template>
 
 <script>

4290badca2e70f2c7864ff1178e942fd6bfd8cd1
Benjamin-Gruber <83592290+Benjamin-Gruber@users.noreply.github.com>
2021-09-27 20:32:25 +0200

    Aufgaben 1 - 2

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,10 +1,20 @@
 <template>
   <v-app>
-    <v-app-bar class="blue lighten-5" app>
-      <span class="text-h4">Drink</span>
-      <v-img class="mx-2" src="../Cola.png" max-height="140" max-width="140" contain></v-img>
+    <v-app-bar class="grey lighten-5" height="74" app>
+      <img class="mx-2" src="images/ff.png" style="height:150px; margin-top:100px" />
+      <span class="text-truncate text-h4 blue-grey--text darken-1" style="font-family: Poppins"
+        >A Dog With a Mission</span
+      >
+      <v-spacer></v-spacer>
+      <router-link active-class="red--text" to="/" exact>Home</router-link>
+      <router-link active-class="red--text" to="/about" exact class="ml-5">About</router-link>
     </v-app-bar>
-    <v-main class="blue"> </v-main>
+    <v-main>
+      <div class="d-flex">
+        <div style="width:250px"></div>
+        <div class="mt-10" style="flex:1"><router-view></router-view></div>
+      </div>
+    </v-main>
   </v-app>
 </template>
 
